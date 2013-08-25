5c416d67780fc8380712d0afa1678fba246afeda
GarimaAhuja <garimaahujaishere@gmail.com>
2013-08-26 04:01:42 +0530

    Added Hibernate code to get column and a servlet to test it

---
+++ api/src/main/java/org/openmrs/module/patientmatching/db/hibernate/HibernateFieldMetadataDAO.java
@@ -0,0 +1,37 @@
+package org.openmrs.module.patientmatching.db.hibernate;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.transaction.annotation.Transactional;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import org.openmrs.module.patientmatching.HibernateConnection;
+
+@Transactional
+public class HibernateFieldMetadataDAO{
+
+	private SessionFactory sessionFactory;
+	protected final Log log = LogFactory.getLog(this.getClass());
+
+	public HibernateFieldMetadataDAO() {
+	}
+
+	public java.util.List<String> getField(String tableName, String columnName)
+	{
+		HibernateConnection hc = new HibernateConnection();
+		sessionFactory = hc.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Query query = session.createSQLQuery("select "+columnName+" from "+tableName);
+		List result = query.list();
+		session.close();
+		return result;
+	}
+
+}
---
+++ omod/src/main/java/org/openmrs/module/patientmatching/web/TestServlet.java
@@ -0,0 +1,45 @@
+package org.openmrs.module.patientmatching.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.openmrs.module.patientmatching.db.hibernate.HibernateFieldMetadataDAO;
+import java.util.List;
+
+public class TestServlet extends HttpServlet {
+
+	private Log log = LogFactory.getLog(getClass());
+	HibernateFieldMetadataDAO hfm = new HibernateFieldMetadataDAO();	
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		String message = "Potato naaa Banananan ";
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+
+		List result = hfm.getField("person","person_id");
+		Object elem = result.get(0);
+		out.println("<h1>" + elem + "</h1>");
+		
+	}
+
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException
+	{
+		doGet(request, response);
+	}
+
+}
